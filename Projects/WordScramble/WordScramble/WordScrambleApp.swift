c569f11f6e40a9ecc53c218135bcf01d69029ffa
Carolane LEFEBVRE <107761189+CarolaneLFBV@users.noreply.github.com>
2022-11-24 15:43:30 +0100

    ✨FEAT: Add WordScramble & Challenges
    WordScramble

---
+++ Projects/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Carolane LEFEBVRE on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
