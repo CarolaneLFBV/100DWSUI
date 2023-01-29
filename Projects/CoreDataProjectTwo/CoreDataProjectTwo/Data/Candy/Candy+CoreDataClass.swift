764df860a6045effd80669ec7b19f2f84b8e6778
CarolaneLFBV <lefebvrecarolane@icloud.com>
2023-01-29 14:28:28 +0100

    ADD: CoreDataProject part two

---
+++ Projects/CoreDataProjectTwo/CoreDataProjectTwo/Data/Candy/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProjectTwo
+//
+//  Created by Carolane LEFEBVRE on 29/01/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ Projects/CoreDataProjectTwo/CoreDataProjectTwo/Data/Country/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProjectTwo
+//
+//  Created by Carolane LEFEBVRE on 29/01/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
