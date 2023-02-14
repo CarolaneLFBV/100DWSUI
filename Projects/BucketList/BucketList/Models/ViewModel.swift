713f8ceee9c56720a89d2ce900e0dae9575796e0
CarolaneLFBV <lefebvrecarolane@icloud.com>
2023-02-14 17:14:18 +0100

    CHANGE: MVVM Structure

--- Projects/BucketList/BucketList/Models/ViewModel.swift
+++ Projects/BucketList/BucketList/Models/ViewModel.swift
@@ -5,4 +5,44 @@
 //  Created by Carolane LEFEBVRE on 14/02/2023.
 //
 
-import Foundation
+import MapKit
+
+extension ContentView {
+    // responsible for running all user interface updates
+    @MainActor class ViewModel: ObservableObject {
+        @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+        @Published private(set) var locations: [Location]
+        @Published var selectedPlace: Location?
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        func addLocation() {
+            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+            locations.append(newLocation)
+        }
+        
+        func update(location: Location) {
+            guard let selectedPlace = selectedPlace else { return }
+            if let index = locations.firstIndex(of: selectedPlace) {
+                locations[index] = location
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection]) // make sure to secure the file
+            } catch {
+                print("Unable to save data")
+            }
+        }
+
+    }
+}
